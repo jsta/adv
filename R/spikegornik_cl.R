a55dd8e3ade7b40b05a5698f93079b91eb22195b
jsta <jstachelek@utexas.edu>
2016-02-10 11:11:28 -0500

    fix namespace issues, create vignette

---
+++ R/spikegornik_cl.R
@@ -0,0 +1,7 @@
+#'@name spikegornik_cl
+#'@title Command-line interface to the spikegornik function
+spikegornik_cl <- function(fpath, outpath){
+	spikegornik(fpath, outpath)
+}
+#args <- commandArgs(trailingOnly = TRUE)
+#spikegornik_cl(args[1], args[2])
